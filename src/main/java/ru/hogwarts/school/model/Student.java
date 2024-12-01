16b72e0eb2130d54cf258ce50b0a256df9bd4f67
maksimfedorov <fedorov-max89@mail.ru>
2024-12-01 14:58:13 +0300

    [lesson134131_task1]: тесты WebMvcTest

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -50,4 +50,8 @@ public class Student {
     public Faculty getFaculty() {
         return faculty;
     }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
 }
